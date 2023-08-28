1cc6a18c5674f7bb352644e7e21cfde135703f77
hedhu <edvv.at@gmail.com>
2023-08-28 10:50:18 -0400

    EsteticaApp

---
+++ src/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'abc.123'
+app.config['MYSQL_DB'] = 'pruebas'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        nombre = request.form['usuario']
+        contraseña = request.form['contraseña']
+        conn = mysql.connection
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM usuarios WHERE nombreUsuario = %s', (nombre,))
+        usuarios = cursor.fetchall()
+
+        if usuarios:
+            for usuario in usuarios:
+                if usuario[2] == contraseña:
+                    print('LOGEADO CORRECTAMENTE')
+                    return redirect(url_for('buscador'))
+                else:
+                    print('CONTRASEÑA INCORRECTA...')
+                    cursor.close()
+                    return render_template('login.html')
+        else:
+            print('USUARIO NO ENCONTRADO...')
+            cursor.close()
+            return render_template('login.html')
+        cursor.close()
+    else: 
+        return render_template('login.html')
+
+@app.route('/buscador')
+def buscador():
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM pacientes')
+    pacientes = cursor.fetchall()
+    npacientes = len(pacientes)
+    return render_template('buscador.html', npacientes=npacientes, pacientes=pacientes)
+
+@app.route('/agregarPaciente', methods = ['POST'])
+def agregarPaciente():
+    if request.method == 'POST':
+        nombrePaciente = request.form['nombrePaciente']
+        edadPaciente = request.form['edadPaciente']
+        fechaNac = request.form['fechaNac']
+        telPaciente = request.form['telPaciente']
+        conn = mysql.connection
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO pacientes (nombrePaciente, edadPaciente, fechaNacimiento, Telefono) VALUES (%s, %s, %s, %s)', (nombrePaciente, edadPaciente, fechaNac, telPaciente))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for('buscador'))
+
+@app.route('/detallesPaciente')
+def detallesPaciente():
+    return render_template('detallesPaciente.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
