367a4a07e04fb9da5de6b2881794ac369839d157
hedhu <edvv.at@gmail.com>
2023-08-29 15:46:39 -0400

    firma y detalles pacientes

---
+++ src/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {var canvas = document.getElementById('signature-pad');
+var signaturePad = new SignaturePad(canvas);
+
+document.getElementById('clear').addEventListener('click', function () {
+    signaturePad.clear();
+});})
